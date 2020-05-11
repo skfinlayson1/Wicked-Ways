63a8c14cc14009ac6a35f24006d08acd853aeaf5
Shane Finlayson <shane_finlayson@yahoo.com>
2020-05-11 16:58:48 -0600

    initial setup

---
+++ client-side/src/app/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+// Components
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <header>
+        <h1>hello</h1>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server-side/configuration/app-config.js
@@ -0,0 +1,32 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+const path = require("path");
+const cors = require("cors");
+const flash = require("flash");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const formData = require("express-form-data");
+const cloudinary = require("cloudinary").v2;
+
+module.exports = {
+
+    init(app, express) {
+        app.use(express.static(path.join(__dirname, "../", "../", "client-side", "build")));
+        app.use(bodyParser.json());
+        app.use(formData.parse());
+        app.use(cors());
+        app.use(session({
+            secret: process.env.SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false,
+            cookie: { maxAge: 1.21e+9 }
+        }))
+        app.use(flash());
+        cloudinary.config({
+            cloud_name: "wicked-ways",
+            api_key: "898855627752421",
+            api_secret: process.env.CLOUDINARY_SECRET
+        })
+
+    }
+
+}
